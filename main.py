d170636bad0a3028206ecd908231aab56327e837
R Hull <rhull9@outlook.com>
2023-06-09 15:17:22 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+from modules import functions
+import time
+
+print("\n--ToDo List App--")
+
+now = time.strftime("%b %d, %Y %I:%M %p")
+print("It is", now)
+while True:
+    user_input = input("\nOptions:\n"
+                       "1 - Add a todo\n"
+                       "2 - Edit a todo\n"
+                       "3 - Complete a todo\n"
+                       "4 - View todos\n"
+                       "5 - Export to file\n"
+                       "Type 'exit' to close program.\n\n").casefold().strip()
+
+    if user_input.startswith("1"):
+        todo = user_input[2:]
+        todos = functions.read_from_file()
+        todos.append(todo + "\n")
+        functions.write_to_file(todos)
+        functions.print_todos(todos)
+    elif user_input.startswith("2"):
+        try:
+            todos = functions.read_from_file()
+            functions.print_todos(todos)
+            edit_todo = int(input("Edit a todo: ")) - 1
+            if len(todos) != 0:
+                todos[int(edit_todo)] = input("Please make your changes: ") + "\n"
+                functions.write_to_file(todos)
+                print("\nUpdated todos:\n")
+                functions.print_todos(todos)
+            else:
+                print("Please have a todo in the list to edit...")
+        except ValueError:
+            print("Not a valid option. Please type the todo's number.")
+            continue
+    elif user_input.startswith("3"):
+        try:
+            todos = functions.read_from_file()
+            functions.print_todos(todos)
+            remove_todo = input("Complete a todo. type 'all' to complete all todos: ").casefold().strip()
+            if remove_todo == "all":
+                todos.clear()
+                functions.write_to_file(todos)
+                print("All todos have been completed!")
+            elif len(todos) != 0:
+                todos.pop(int(remove_todo) - 1)
+                functions.write_to_file(todos)
+                print(f"\nTodo, {remove_todo} was removed from the list.")
+            else:
+                print("\nPlease have a todo in the list to remove...")
+        except IndexError:
+            print(f"\nTodo, {remove_todo} does not exist. Please choose a todo that exists.")
+            continue
+        except ValueError:
+            print("\nPlease type in a number to complete the todo.")
+    elif user_input.startswith("4"):
+        print("View Todos:\n")
+        todos = functions.read_from_file()
+        functions.print_todos(todos)
+    elif user_input.startswith("5"):
+        todos = functions.read_from_file()
+        export_choice = input("Export to File \n Choose a format to export to (type in 'cancel' to cancel): \n"
+                              "1 - .csv\n"
+                              "2 - .json\n"
+                              "3 - .xls\n\n").casefold().strip()
+        if export_choice == "cancel":
+            continue
+        elif export_choice == "1":
+            functions.export_to_file(export_choice, todos)
+        elif export_choice == "2":
+            functions.export_to_file(export_choice, todos)
+        elif export_choice == "3":
+            functions.export_to_file(export_choice, todos)
+        else:
+            print("Export for file type is not found...")
+    elif user_input.startswith("exit"):
+        break
+    else:
+        print("Please choose a valid option...")
---
+++ modules/functions.py
@@ -0,0 +1,48 @@
+import csv
+import json
+
+from xlwt import Workbook
+
+
+def print_todos(items):
+    """Prints the list of todos"""
+    for index, item in enumerate(items):
+        item = item.strip('\n')
+        print(f"{index + 1}-{item}")
+
+
+def export_to_file(filetype, items):
+    """Exports todos into .csv, .json, or .xls file formats"""
+    filepath = input("Export to File \nPlease specify the file path to save to: ")
+    if filetype.startswith("1"):
+        with open(filepath, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(items)
+    elif filetype.startswith("2"):
+        with open(filepath, 'w') as file:
+            json.dump(items, file)
+    elif filetype.startswith("3"):
+        wb = Workbook()
+        sheet = wb.add_sheet("Todos 1", cell_overwrite_ok=True)
+        for i in range(0, len(items)):
+            sheet.write(i, 0, items[i])
+        wb.save(filepath)
+    else:
+        print("Export for file type is not found...")
+
+
+def read_from_file(filepath="files/todos.txt"):
+    """Reads todos from .txt file"""
+    with open(filepath, 'r') as file:
+        items = file.readlines()
+    return items
+
+
+def write_to_file(items, filepath="files/todos.txt"):
+    """Writes todos to .txt file"""
+    with open(filepath, 'w') as file:
+        file.writelines(items)
+
+
+if __name__ == "__main__":
+    print("Hello from functions")
