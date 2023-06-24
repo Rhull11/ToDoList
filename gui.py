91a719bbe041a3adf583a2f6f4ec6ddcd0f8ba3f
rhull <rhull9@outlook.com>
2023-06-24 16:39:01 -0600

    Added way to export to different file types.

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ add_todo_button = gui.Button("Add")
 todos_box = gui.Listbox(values=functions.read_from_file(),
                         key="items",
                         enable_events=True,
-                        size=(45, 10), expand_x=True, expand_y=True)
+                        size=(65, 15), expand_x=True, expand_y=True)
 edit_button = gui.Button("Edit")
 complete_button = gui.Button("Complete")
 export_to_file_button = gui.Button("Export to file")
@@ -19,20 +19,55 @@ window = gui.Window("Todo App",
                             [add_todo_button, edit_button, complete_button, export_to_file_button]],
                     font=("Helvetica", 15), resizable=True)
 
+
+def save_to_file(list_box_items):
+    """Window popup to save file"""
+    layout = [[gui.Text('Choose a file type'),
+               gui.Button(".csv"),
+               gui.Button(".json"),
+               gui.Button(".xls")]]
+    file_window = gui.Window('File Type', layout, finalize=True, font=("Helvetica", 15))
+
+    file_event, file_values = file_window.read()
+
+    match file_event:
+        case ".xls":
+            functions.export_to_excel("files/todos.xls", list_box_items)
+        case ".csv":
+            functions.export_to_csv("files/todos.csv", list_box_items)
+        case ".json":
+            functions.export_to_json("files/todos.json", list_box_items)
+
+    file_window.close()
+
+
 while True:
     event, values = window.read()
+    print(event)
+    print(values)
     match event:
         case "Add":
             todos = functions.read_from_file()
             todos.append(values["todo"] + "\n")
             functions.write_to_file(todos)
+            window["items"].update(values=todos)
         case "Edit":
             todo_to_edit = values["items"][0]
             new_todo = values["todo"]
             todos = functions.read_from_file()
-            todos[todos.index(todo_to_edit)] = new_todo
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
             functions.write_to_file(todos)
             window["items"].update(values=todos)
+        case "Complete":
+            remove_todo = values["items"][0]
+            todos = functions.read_from_file()
+            todos.pop(todos.index(remove_todo))
+            functions.write_to_file(todos)
+            window["items"].update(values=todos)
+        case "Export to file":
+            todos = functions.read_from_file()
+            save_to_file(todos)
         case "items":
             window["todo"].update(value=values["items"][0])
         case gui.WIN_CLOSED:
--- modules/functions.py
+++ modules/functions.py
@@ -12,24 +12,44 @@ def print_todos(items):
         print(f"{index + 1}-{item}")
 
 
-def export_to_file(filetype, items):
-    """Exports todos into .csv, .json, or .xls file formats"""
-    filepath = input("Export to File \nPlease specify the file path to save to: ")
-    if filetype.startswith("1"):
-        with open(filepath, 'w') as file:
-            writer = csv.writer(file)
-            writer.writerow(items)
-    elif filetype.startswith("2"):
-        with open(filepath, 'w') as file:
-            json.dump(items, file)
-    elif filetype.startswith("3"):
-        wb = Workbook()
-        sheet = wb.add_sheet("Todos 1", cell_overwrite_ok=True)
-        for i in range(0, len(items)):
-            sheet.write(i, 0, items[i])
-        wb.save(filepath)
-    else:
-        print("Export for file type is not found...")
+# def export_to_file(filetype, items):
+#     """Exports todos into .csv, .json, or .xls file formats"""
+#    filepath = input("Export to File \nPlease specify the file path to save to: ")
+#     if filetype.startswith("1"):
+#         with open(filepath, 'w') as file:
+#             writer = csv.writer(file)
+#             writer.writerow(items)
+#     elif filetype.startswith("2"):
+#         with open(filepath, 'w') as file:
+#             json.dump(items, file)
+#     elif filetype.startswith("3"):
+#         wb = Workbook()
+#         sheet = wb.add_sheet("Todos 1", cell_overwrite_ok=True)
+#         for i in range(0, len(items)):
+#         sheet.write(i, 0, items[i])
+#         wb.save(filepath)
+#     else:
+#         print("Export for file type is not found...")
+
+
+def export_to_excel(filepath, items):
+    """Exports todos to .xls file"""
+    wb = Workbook()
+    sheet = wb.add_sheet("Todos 1", cell_overwrite_ok=True)
+    for i in range(0, len(items)):
+        sheet.write(i, 0, items[i])
+    wb.save(filepath)
+
+
+def export_to_json(filepath, items):
+    with open(filepath, 'w') as file:
+        json.dump(items, file)
+
+
+def export_to_csv(filepath, items):
+    with open(filepath, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(items)
 
 
 def read_from_file(filepath=FILEPATH):
