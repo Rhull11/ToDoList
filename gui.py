eafe011d39dd26293183eb2cc72266a9fb0ed96b
rhull <rhull9@outlook.com>
2024-06-13 03:47:48 -0600

    Removed unused code in functions.py, moving todos.txt to main folder.

--- gui.py
+++ gui.py
@@ -1,6 +1,13 @@
 from modules import functions
 import PySimpleGUI as gui
+import time
+import os
 
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
+
+clock = gui.Text("", key="clock")
 label = gui.Text("Type in a todo")
 input_box = gui.InputText(tooltip="Enter todo", key="todo", expand_x=True)
 add_todo_button = gui.Button("Add")
@@ -11,13 +18,17 @@ todos_box = gui.Listbox(values=functions.read_from_file(),
 edit_button = gui.Button("Edit")
 complete_button = gui.Button("Complete")
 export_to_file_button = gui.Button("Export to file")
+settings_button = gui.Button("Settings")
 exit_button = gui.Button("Exit")
+refresh_button = gui.Button("Refresh")
 
 window = gui.Window("Todo App",
-                    layout=[[todos_box],
+                    layout=[[clock],
+                            [todos_box],
                             [label],
                             [input_box],
-                            [add_todo_button, edit_button, complete_button, export_to_file_button, exit_button]],
+                            [add_todo_button, edit_button, complete_button,
+                             settings_button, refresh_button, exit_button]],
                     font=("Helvetica", 15), resizable=True)
 
 
@@ -42,10 +53,46 @@ def save_to_file(list_box_items):
     file_window.close()
 
 
+def open_themes_popup():
+    themes = gui.theme_list()
+    layout = [[[gui.Text('Choose a Theme')],
+              gui.Combo(themes, key="-THEMES-", enable_events=True)]]
+    themes_window = gui.Window('Themes', layout, font=("Helvetica", 15))
+
+    while True:
+        themes_event, themes_values = themes_window.read()
+        print(themes_values)
+        if themes_event == gui.WIN_CLOSED:
+            break
+        gui.theme(themes_values["-THEMES-"][0])
+
+    themes_window.close()
+
+
+def open_settings_popup(list_box_items):
+    layout = [[gui.Button("Export to File"),
+              gui.Button("Themes"),
+              gui.Button("About")]]
+    settings_window = gui.Window('Settings', layout, font=("Helvetica", 15))
+
+    while True:
+        settings_event, settings_value = settings_window.read()
+        match settings_event:
+            case "Export to File":
+                save_to_file(list_box_items)
+            case "Themes":
+                open_themes_popup()
+            case "About":
+                gui.popup("Todo List app \n\n R.Hull \n\n 2023", font=("Helvetica", 15), title="About")
+            case gui.WIN_CLOSED:
+                break
+
+    settings_window.close()
+
+
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %I:%M %p"))
     match event:
         case "Add":
             todos = functions.read_from_file()
@@ -53,23 +100,32 @@ while True:
             functions.write_to_file(todos)
             window["items"].update(values=todos)
         case "Edit":
-            todo_to_edit = values["items"][0]
-            new_todo = values["todo"]
-            todos = functions.read_from_file()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_to_file(todos)
-            window["items"].update(values=todos)
+            try:
+                todo_to_edit = values["items"][0]
+                new_todo = values["todo"]
+                todos = functions.read_from_file()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_to_file(todos)
+                window["items"].update(values=todos)
+                window["warning_message"].update(value="")
+            except IndexError:
+                gui.popup("Please select a todo", font=("Helvetica", 15))
         case "Complete":
-            remove_todo = values["items"][0]
+            try:
+                remove_todo = values["items"][0]
+                todos = functions.read_from_file()
+                todos.pop(todos.index(remove_todo))
+                functions.write_to_file(todos)
+                window["items"].update(values=todos)
+                window["todo"].update(value="")
+            except IndexError:
+                gui.popup("Please select a todo", font=("Helvetica", 15))
+        case "Settings":
             todos = functions.read_from_file()
-            todos.pop(todos.index(remove_todo))
-            functions.write_to_file(todos)
-            window["items"].update(values=todos)
-            window["todo"].update(value="")
-        case "Export to file":
-            todos = functions.read_from_file()
-            save_to_file(todos)
+            open_settings_popup(todos)
+        case "Refresh":
+            window.refresh()
         case "items":
             window["todo"].update(value=values["items"][0])
         case "Exit":
@@ -78,3 +134,6 @@ while True:
             break
 
 window.close()
+
+if __name__ == "__main__":
+    print("Hello from functions")
--- modules/functions.py
+++ modules/functions.py
@@ -3,33 +3,13 @@ import json
 
 from xlwt import Workbook
 
-FILEPATH = "files/todos.txt"
+FILEPATH = "todos.txt"
 
 def print_todos(items):
     """Prints the list of todos"""
     for index, item in enumerate(items):
         item = item.strip('\n')
         print(f"{index + 1}-{item}")
-
-
-# def export_to_file(filetype, items):
-#     """Exports todos into .csv, .json, or .xls file formats"""
-#    filepath = input("Export to File \nPlease specify the file path to save to: ")
-#     if filetype.startswith("1"):
-#         with open(filepath, 'w') as file:
-#             writer = csv.writer(file)
-#             writer.writerow(items)
-#     elif filetype.startswith("2"):
-#         with open(filepath, 'w') as file:
-#             json.dump(items, file)
-#     elif filetype.startswith("3"):
-#         wb = Workbook()
-#         sheet = wb.add_sheet("Todos 1", cell_overwrite_ok=True)
-#         for i in range(0, len(items)):
-#         sheet.write(i, 0, items[i])
-#         wb.save(filepath)
-#     else:
-#         print("Export for file type is not found...")
 
 
 def export_to_excel(filepath, items):
@@ -64,6 +44,3 @@ def write_to_file(items, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(items)
 
-
-if __name__ == "__main__":
-    print("Hello from functions")
